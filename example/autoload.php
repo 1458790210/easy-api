7a043e883c49f861c1a63c34ec71ea930b3e6985
ryan <123123cc>
2020-10-26 21:34:20 +0800

    edit

--- example/autoload.php
+++ example/autoload.php
@@ -3,11 +3,11 @@
 function classLoader($class)
 {
     $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    $file = __DIR__ . '/src/' . $path . '.php';
-
+    $file = str_replace("\\example", "", dirname(__FILE__)) . '/src/' . $path . '.php';;
     if (file_exists($file)) {
         require_once $file;
     }
 }
+
 spl_autoload_register('classLoader');
 
